45506f38fa0e0e02f647f9846d6d57ac4010fcb5
Cam desktop <cdlindsa@ualberta.ca>
2020-05-11 16:35:24 -0700

    Remove Courses

--- Courses/SENG330/Assignment1/src/main/java/Monster.java
+++
@@ -1,6 +0,0 @@
-public final class Monster extends Character{
-
-    public Monster(int initiativeModifier, int hitPoints, int attackModifier, String name) {
-        super(initiativeModifier, hitPoints, attackModifier, name);
-    }
-}
--- Courses/SENG330/Assignment1/src/main/java/Player.java
+++
@@ -1,6 +0,0 @@
-public final class Player extends Character{
-
-    public Player(int initiativeModifier, int hitPoints, int attackModifier, String name) {
-        super(initiativeModifier, hitPoints, attackModifier, name);
-    }
-}
